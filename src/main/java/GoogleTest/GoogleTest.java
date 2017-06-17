a383b7415ebda8f7c66d0b21b7b54bc0b4a69adb
kartheemathi <kartheemathi@gmail.com>
2017-06-17 11:11:56 +0530

    Suit commit

--- src/main/java/GoogleTest/GoogleTest.java
+++ src/main/java/GoogleTest/GoogleTest.java
@@ -1,5 +1,24 @@
 package GoogleTest;
 
-public class GoogleTest {
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleTest {
+    @Test
+	public void logintest() throws IOException
+{
+	WebDriver wd=new FirefoxDriver();
+	wd.manage().window().maximize();
+	wd.get("https://www.google.co.in/?gfe_rd=cr&ei=44E7WZXIJYry8Afq64KQDw&gws_rd=ssl");
+	File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(src, new File("C:\\workspace\\Mars\\GoogleTest\\Screenshot"));
+	wd.quit();
+}
 }
